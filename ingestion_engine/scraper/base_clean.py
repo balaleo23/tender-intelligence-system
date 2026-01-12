c00ccf795edb5593327259615f8f1059f98ed92b
Bala <balamurugand@hexamapsolutions.com>
2026-01-12 21:25:22 +0530

    feat: integrated with the download page for the tender link click need to fix the captcha step for the next one

--- ingestion_engine/scraper/base_clean.py
+++ ingestion_engine/scraper/base_clean.py
@@ -2,7 +2,7 @@ import json
 from datetime import datetime
 import logging
 from playwright.sync_api import sync_playwright, Page, Locator
-
+from urllib.parse import urljoin, urlparse, parse_qs, urlencode
 # Logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -21,7 +21,8 @@ class Scrapper:
     def open_load_content(self) -> Page:
         self.p = sync_playwright().start()
         self.browser = self.p.chromium.launch(headless=False)
-        self.page = self.browser.new_page()
+        self.context = self.browser.new_context()
+        self.page = self.context.new_page() 
 
         self.page.goto(URL, wait_until="domcontentloaded", timeout=60000)
         self.page.wait_for_timeout(3000)
@@ -31,7 +32,6 @@ class Scrapper:
 
         return self.page
 
-
     def extract_rows(self, page: Page):
         page_number = 1
 
@@ -40,9 +40,10 @@ class Scrapper:
 
            tender_table = page.locator('//*[@id="table"]')
 
-           if tender_table is None or page_number == 10:
+           if tender_table is None or page_number == 3: # sample for ten pages for demo
                break
-
+               
+           page.wait_for_timeout(2000)
            tender_content = tender_table.locator("tr")
            print("Rows Count:", tender_content.count())
            page.wait_for_timeout(2000)
@@ -83,8 +84,46 @@ class Scrapper:
             # for col_index  in range(min(cells_count), len(headers)):
             #     row_data[headers[col_index]] = cells.nth(col_index).inner_text().strip()
             for col_index in range(min(cells_count,len(headers))):
+                # tender link open
                 cell_value = cells.nth(col_index).inner_text().strip()
                 row_data[headers[col_index]] = cell_value
+                
+                if col_index == 4:
+                    # page.get_by_text(cell_value)
+                    title_text = cells.nth(4).inner_text().strip()
+                    # clean_text = title_text.split("]")[0].replace("[", "").strip()
+
+                    # page.get_by_text(clean_text).click()
+                    # # page.get_by_role("link", name=clean_text, exact=True).click()
+                    # page.wait_for_load_state("domcontentloaded")
+
+                    title_cell = cells.nth(4)
+                    tender_href = title_cell.locator("a").get_attribute("href")
+
+                    # print(f"tender_href : {tender_href}")
+                    # print(f"page_Url : {self.page.url}")
+
+                    full_url = urljoin(self.page.url, tender_href)
+                    print(full_url)
+
+                    new_page = self.page.context.new_page()
+                    new_page.goto(full_url, wait_until="domcontentloaded")
+
+                    download_link_cnt = new_page.locator('//*[@id="DirectLink_8"]')
+                    # download_link_cnt.click()
+                    
+
+                    print(download_link_cnt)
+                    # need to handle the captch logic and saving logic for the same
+
+                    # print("Tender URL:", tender_href)
+                    # page.goto(tender_href, wait_until="domcontentloaded")
+                    # page.wait_for_timeout(2000)
+                    #scraping_logic here
+
+                    # page.go_back(wait_until="domcontentloaded")
+                    page.wait_for_timeout(2000)
+                    
             
             if row_data:
                 self.data.append(row_data)
@@ -112,8 +151,3 @@ if __name__ == "__main__":
     scrapper.extract_rows(page)
     scrapper.form_json()
     scrapper.close()
-           
-            
-
-
-        
